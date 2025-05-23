8d64a72a36af366e961e1930ccf93351e0d73632
Pantawat <pantawat_b@cmu.ac.th>
2025-05-23 10:13:19 +0700

    tailwindconfig

--- crud-intern_api/src/views/HomeView.vue
+++ crud-intern_api/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
+<script lang="ts"></script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="p-4 bg-blue-500 text-white text-xl rounded-lg">Hello Tailwind!</div>
+  <div class="text-red-500 text-2xl">Hello</div>
 </template>
