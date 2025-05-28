f33c2e06f5c063b235adeebf6de5d0b6a5e5061d
Pantawat <pantawat_b@cmu.ac.th>
2025-05-28 13:55:59 +0700

    profile_add

--- crud-intern_api/src/views/HomeView.vue
+++ crud-intern_api/src/views/HomeView.vue
@@ -164,6 +164,11 @@
               ID
             </th>
             <th
+              class="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Profile
+            </th>
+            <th
               class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
             >
               Title
@@ -196,6 +201,37 @@
         <tbody class="bg-white divide-y divide-gray-200">
           <tr v-for="blog in sortedData" :key="blog.id" class="hover:bg-gray-50">
             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ blog.id }}</td>
+            <!-- Profile ↓↓↓ -->
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+              <!-- ถ้ามีรูปภาพให้แสดงรูป ถ้าไม่มีให้แสดงรูป default -->
+              <div class="flex items-center justify-center">
+                <div v-if="blog.Img?.url" class="w-12 h-12 relative">
+                  <img
+                    :src="`https://exam-api.dev.mis.cmu.ac.th${blog.Img.url}`"
+                    class="w-full h-full object-cover rounded-lg"
+                  />
+                </div>
+                <div
+                  v-else
+                  class="w-12 h-12 bg-gray-200 rounded-lg flex items-center justify-center"
+                >
+                  <svg
+                    class="w-6 h-6 text-gray-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </td>
+            <!-- Profile ^^^^^ -->
             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ blog.title }}</td>
             <td class="px-6 py-4 text-sm text-gray-900">
               <div class="max-w-xs overflow-hidden text-ellipsis">{{ blog.content }}</div>
